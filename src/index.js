f852602cc59dab7ade55d52b66c78858a57e07fc
dingod <8838209+dingallen216@user.noreply.gitee.com>
2023-03-20 21:14:05 +0800

    使用cli-table库实现json数据的表格输出

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const vorpal = require('vorpal')();
 const BlockChain = require('./blockchain');
+const Table = require('cli-table');
 
 const blockchain = new BlockChain();
+
+const formatLog = (data) => {
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+    const first = data[0];
+    const headers = Object.keys(first);
+    const table = new Table({
+        head: headers
+    });
+    const result = data.map(item => {
+        return headers.map(key => item[key]);
+    });
+    table.push(...result);
+    console.log(table.toString());
+}
 vorpal.command('hello', '向这个程序问好，你会得到友好的回应').action(function (args, callback) {
     this.log('你好，帅哥!');
     callback();
@@ -12,14 +29,14 @@ vorpal.command('mine', '挖矿').action(function (args, callback) {
     const newBlock = blockchain.mine();
     if (newBlock) {
         this.log('挖矿成功，新区块为：');
-        this.log(newBlock);
+        formatLog(newBlock);
     }
     callback();
 });
 
 // 实现查看区块链的命令行
 vorpal.command('chain', '查看区块链').action(function (args, callback) {
-    this.log(blockchain.blockchain);
+    formatLog(blockchain.blockchain);
     callback();
 });
 
